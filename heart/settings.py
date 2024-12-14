3c5ff458a4137bc078b1e6195da33a0467ddd349
HoaiNampro <namn2591@gmail.com>
2024-12-14 17:16:59 +0700

    fix allowed host

--- heart/settings.py
+++ heart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2l737brm99vy$k5h7o!a4ezr33$ewmyf+9(p(99+w7g0m2(ri4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
